ab6e59b237d596a748d3ce637e8e0942c48ce0cc
Junie Mariam Varghese <juniemariam@gmail.com>
2024-10-01 12:23:45 -0700

    Add files via upload

---
+++ build/precache-manifest.25fb090667ca57983b9bedf98f469c2f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "c700b0e02382fa801dd4a59be163f0b8",
+    "url": "/static/media/analytics.c700b0e0.svg"
+  },
+  {
+    "revision": "e56101af1b19b012960f",
+    "url": "/static/css/main.1299371c.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "46f8b188eff8e1bf1546",
+    "url": "/static/js/2.16ede342.chunk.js"
+  },
+  {
+    "revision": "650cdc57dc0be3a71cc230a760215df5",
+    "url": "/static/media/deleteIcon.650cdc57.svg"
+  },
+  {
+    "revision": "ed718a5fe4c2c476c20d3e21a9f22868",
+    "url": "/static/media/plus.ed718a5f.svg"
+  },
+  {
+    "revision": "e56101af1b19b012960f",
+    "url": "/static/js/main.05a464ba.chunk.js"
+  },
+  {
+    "revision": "ce49021a462f4334c4e6dadbdf9707f4",
+    "url": "/static/media/phone.ce49021a.svg"
+  },
+  {
+    "revision": "ab11192fd42d9789adcb7eb46dd82bf5",
+    "url": "/static/media/heart.ab11192f.svg"
+  },
+  {
+    "revision": "17ee361ccd9f9cfc92c6ac135cca2380",
+    "url": "/static/media/mapIcon.17ee361c.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "533245f701cc2a27e93c6cac389adde5",
+    "url": "/index.html"
+  }
+];
